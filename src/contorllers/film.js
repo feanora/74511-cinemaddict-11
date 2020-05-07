97ef282a5c205a483277a80475772165a34fb6b6
Yulia Akutina <akutinayulia@gmail.com>
2020-05-07 04:09:09 +0300

    Создает контроллер фильма; перенастраивает контроллер страницы на работу с контроллером фильма

---
+++ src/contorllers/film.js
@@ -0,0 +1,59 @@
+import {generateComments} from "../mock/comment";
+import {remove, render} from "../utils/render";
+import CommentComponent from "../components/comment";
+import FilmCardComponent from "../components/film-card";
+import FilmPopupComponent from "../components/film-popup";
+import {RenderPosition} from "../const";
+
+const renderComments = (film, commentsContainer) => {
+  const comments = generateComments(film.commentsCount);
+  comments.slice(0, comments.length).forEach((comment) => {
+    render(commentsContainer, new CommentComponent(comment));
+  });
+};
+
+export default class FilmController {
+  constructor(container) {
+    this._container = container;
+    this._filmCardComponent = null;
+    this._filmPopupComponent = null;
+    this._popupEscKeyDownHandler = this._popupEscKeyDownHandler.bind(this);
+  }
+
+  render(film) {
+    this._filmCardComponent = new FilmCardComponent(film);
+    render(this._container, this._filmCardComponent);
+
+    this._filmCardComponent.setFilmCardElementsClickHandler((evt) => {
+      const target = evt.target.closest(`.film-card__poster, .film-card__title, .film-card__comments`);
+      if (target) {
+        this._renderPopup(film);
+      }
+    });
+  }
+
+  _renderPopup(film) {
+    this._filmPopupComponent = new FilmPopupComponent(film);
+    const siteFooterElement = document.querySelector(`.footer`);
+    render(siteFooterElement, this._filmPopupComponent, RenderPosition.AFTEREND);
+    const commentsContainerElement = this._filmPopupComponent.getElement().querySelector(`.film-details__comments-list`);
+    renderComments(film, commentsContainerElement);
+    this._filmPopupComponent.setPopupCloseElementClickHandler(() => {
+      this._closePopup();
+    });
+    document.addEventListener(`keydown`, this._popupEscKeyDownHandler);
+  }
+
+  _closePopup() {
+    document.removeEventListener(`keydown`, this._popupEscKeyDownHandler);
+    remove(this._filmPopupComponent);
+  }
+
+  _popupEscKeyDownHandler(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+    if (isEscKey) {
+      this._closePopup();
+    }
+  }
+
+}
--- src/contorllers/page.js
+++ src/contorllers/page.js
@@ -1,65 +1,17 @@
-import CommentComponent from "../components/comment.js";
 import FilmsBlockComponent from "../components/films-block.js";
-import FilmCardComponent from "../components/film-card.js";
-import FilmPopupComponent from "../components/film-popup.js";
 import FilmsExtraBlockComponent from "../components/films-extra-block.js";
 import SortComponent from "../components/sort.js";
 import ShowMoreButtonComponent from "../components/show-more-button.js";
-import {generateComments} from "../mock/comment.js";
-import {ExtraFilmsTitles, FilmCardsCount, RenderPosition} from "../const.js";
+import {ExtraFilmsTitles, FilmCardsCount} from "../const.js";
 import {getSortedFilms} from "../utils/common.js";
-import {remove, removeElement, render} from "../utils/render.js";
-
-const renderComments = (film, commentsContainer) => {
-  const comments = generateComments(film.commentsCount);
-  comments.slice(0, comments.length).forEach((comment) => {
-    render(commentsContainer, new CommentComponent(comment));
-  });
-};
-
-const renderFilmCard = (filmsListElement, film) => {
-  const filmCardComponent = new FilmCardComponent(film);
-  render(filmsListElement, filmCardComponent);
-  const renderPopup = () => {
-    closeIfPopupOpen();
-    const filmPopupComponent = new FilmPopupComponent(film);
-    const siteFooterElement = document.querySelector(`.footer`);
-    render(siteFooterElement, filmPopupComponent, RenderPosition.AFTEREND);
-    const commentsContainerElement = filmPopupComponent.getElement().querySelector(`.film-details__comments-list`);
-    renderComments(film, commentsContainerElement);
-    const popupEscKeyDownHandler = (evt) => {
-      const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-      if (isEscKey) {
-        closePopup();
-      }
-    };
-    const closePopup = () => {
-      document.removeEventListener(`keydown`, popupEscKeyDownHandler);
-      remove(filmPopupComponent);
-    };
-    filmPopupComponent.setPopupCloseElementClickHandler(() => {
-      closePopup();
-    });
-    document.addEventListener(`keydown`, popupEscKeyDownHandler);
-  };
-  const closeIfPopupOpen = () => {
-    const popupElement = document.querySelector(`.film-details`);
-    if (popupElement) {
-      removeElement(popupElement);
-    }
-  };
-
-  filmCardComponent.setFilmCardElementsClickHandler((evt) => {
-    const target = evt.target.closest(`.film-card__poster, .film-card__title, .film-card__comments`);
-    if (target) {
-      renderPopup();
-    }
-  });
-};
+import {remove, render} from "../utils/render.js";
+import FilmController from "./film.js";
 
 const renderFilmsList = (filmsContainer, films) => {
-  films.forEach((film) => {
-    renderFilmCard(filmsContainer, film);
+  return films.map((film) => {
+    const filmController = new FilmController(filmsContainer);
+    filmController.render(film);
+    return filmController;
   });
 };
 
